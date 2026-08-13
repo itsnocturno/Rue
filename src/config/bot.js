d37dc87bc4633885874dc8a393a5cbacafef5d6e
itsnocturno <ruefully991@gmail.com>
2026-08-13 10:17:13 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Trying to not hang myself",     // this is what people actually see
+        state: "You deserve to be happy.",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
