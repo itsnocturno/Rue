65dbd3630dd11b26b19e2b603319c9bb67902e99
itsnocturno <ruefully991@gmail.com>
2026-08-27 15:12:57 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "joe mama 🤣🤣",     // this is what people actually see
+        state: "Welcome New Shadow Folk",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
