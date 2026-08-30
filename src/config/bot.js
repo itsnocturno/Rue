11330a82c4257edbd960cdf432107bc018ff4959
itsnocturno <ruefully991@gmail.com>
2026-08-30 19:27:09 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -175,7 +175,7 @@ export const botConfig = {
     baseBankCapacity: 100000000,
 
     // Daily reward amount.
-    dailyAmount:  U+221E,
+    dailyAmount:  9999999,
 
     // Work command random payout range.
     workMin: 10,
